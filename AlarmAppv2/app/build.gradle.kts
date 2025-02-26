c8e4a108741d2aa9146e1f71efaabecf6e491fd1
yousuf-git <hackerbest052@gmail.com>
2025-02-26 16:54:17 +0500

    minor: build related files

--- AlarmAppv2/app/build.gradle.kts
+++ AlarmAppv2/app/build.gradle.kts
@@ -33,6 +33,10 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
+
+    viewBinding {
+        enable = true
+    }
 }
 
 dependencies {
@@ -42,6 +46,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.gson)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
